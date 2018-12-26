e2a731778427b6a6f6382b8bc21447916d8402be
Khori Hutama <khori@student.uns.ac.id>
2018-12-26 22:18:26 +0700

    edit view backend

--- backend/views/booking/view.php
+++ backend/views/booking/view.php
@@ -6,13 +6,13 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\Booking */
 
-$this->title = $model->id;
+$this->title = 'Booking #'. $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Bookings', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="booking-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Detail</h1>
 
     <!-- <p>
         <= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
@@ -30,13 +30,46 @@ $this->params['breadcrumbs'][] = $this->title;
         'attributes' => [
             // 'id',
             // 'id_pemesan',
-            'id_lapangan',
+            [
+              'label' => 'Lapangan',
+              'attribute'=> 'lapangan.nama',
+            ],
+
             'nama',
             'no_hp',
-            'tanggal_main',
-            'waktu_mulai',
-            'waktu_selesai',
-            'bukti_transfer',
+            [
+                'class'=>'kartik\grid\DataColumn',
+                'attribute'=>'tanggal_main',
+                'value' => function ($model) {
+                  if ($model->tanggal_main == 0000-00-00) {
+                    return "Belum terisi";
+                    // code
+                  }
+                        return date('d-m-Y', strtotime($model->tanggal_main));
+                 }
+            ],
+            [
+                'class'=>'kartik\grid\DataColumn',
+                'label' => 'Mulai Main',
+                'attribute'=>'waktu_mulai',
+                'value' => function ($model) {
+                        return date('H:i:s', strtotime($model->waktu_mulai));
+                 }
+            ],
+            [
+                'class'=>'kartik\grid\DataColumn',
+                'label' => 'Selesai Main',
+                'attribute'=>'waktu_selesai',
+                'value' => function ($model) {
+                        return date('H:i:s', strtotime($model->waktu_selesai));
+                 }
+            ],
+            [
+                'label'=>'Bukti Transfer',
+                'attribute'=>'bukti_transfer',
+                'value' => Yii::getAlias('@sipenful/'. $model->bukti_transfer),
+                'format' => ['image', ['width' => '300px', 'height' => 'auto']],
+            ],
             'no_rek_transfer',
             'nama_rek_transfer',
             // 'created_at',
